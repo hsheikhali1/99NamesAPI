39753848a0790a96e3c565ad66a292fe65d9def0
Harun Sheikhali <harun.sheikhali@agilebits.com>
2022-03-09 11:58:40 -0500

    adding base requirements

--- routes/namesRoutes.js
+++ routes/namesRoutes.js
@@ -1,21 +1,25 @@
-const express = require("express")
-const { getAllNames, getSpecificName, getARandomName, getRange } = require("../controllers/namesController")
+const express = require("express");
+const {
+  getAllNames,
+  getSpecificName,
+  getARandomName,
+  getRange,
+} = require("../controllers/namesController");
 
 const router = express.Router();
 
 // this call gets all the 99 names
-router.get("/api/names", getAllNames)
-// this call gets a random name 
-router.get("/api/name/random", getARandomName)
+router.get("/api/names", getAllNames);
+// this call gets a random name
+router.get("/api/name/random", getARandomName);
 // this call gets a specific name based on the idea - the names are in order so "0" would get the name Allah
-router.get("/api/name/:id", getSpecificName)
+router.get("/api/name/:id", getSpecificName);
 // this will call the range of names specified by the user - a list chosen by user starting from range to other
-router.get("/api/names/range/:id,:id2", getRange)
+router.get("/api/names/range/:id,:id2", getRange);
 // these calls get a random name
-router.get("/", getARandomName)
-router.get("*", getARandomName)
-
+router.get("/", getARandomName);
+router.get("*", getARandomName);
 
 module.exports = {
-    routes: router
-}
+  routes: router,
+};
