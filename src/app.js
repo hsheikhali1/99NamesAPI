73511bc32fb726854951db42f2483341c6b53a9f
Harun Sheikhali <sufisheikhali@gmail.com>
2022-03-19 18:20:59 -0400

    base for swaggerui

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import namesRoutes from './routes/namesRoutes.js';
 import rateLimiterUsingThirdParty from './middlewares/rateLimiter.js';
+import swaggerDocs from '../swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/', rateLimiterUsingThirdParty, namesRoutes);
 
